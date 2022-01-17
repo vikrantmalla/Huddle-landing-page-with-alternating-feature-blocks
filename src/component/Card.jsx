b79c619270e4ad33da2661f2aa9582118e6e4d61
Vikrant Malla <vikrantmalla999@gmail.com>
2022-01-17 15:18:01 +0545

    add screenshot

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -8,11 +8,11 @@ const Card = () => {
                     data.map((details) => {
                         const { id, order, img, alt, title, para } = details;
                         return (
-                            <article className="card bg-white-500 p-4 grid grid-rows-1 justify-center items-center gap-4 md:grid-cols-2  lg:px-20 " key={details.id}>
+                            <article className="card bg-white-500 p-4 grid grid-rows-1 justify-center items-center gap-4 md:grid-cols-2  lg:px-20 " key={id}>
                                 <img className={`md:${order} "p-4"`} src={img} alt={alt} />
                                 <div className="text-center p-6 md:text-left">
                                     <h1 className="font-Poppins font-semibold text-lg mb-4 sm:text-xl">{title}</h1>
-                                    <p className='mb-3 text-GrayishBlue font-OpenSans font-normal text-sm sm:text-base md:mb-0 '>{details.para}</p>
+                                    <p className='mb-3 text-GrayishBlue font-OpenSans font-normal text-sm sm:text-base md:mb-0 '>{para}</p>
                                 </div>
                             </article>
                         )
